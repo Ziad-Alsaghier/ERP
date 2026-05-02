e8ee8370ad8ac3f7a2dfb3e25ec553f3cd3eb783
Ziad-Alsaghier <ziadm57@yahoo.com>
2026-05-02 17:12:46 +0300

    Set default debug mode to false in app configuration

--- config/app.php
+++ config/app.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', $debug),
+    'debug' => (bool) env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------

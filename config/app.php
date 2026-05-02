702457f3a64e4fc54aaf434728bb19fab3fdc71b
Ziad-Alsaghier <ziadm57@yahoo.com>
2026-05-02 17:54:43 +0300

    Update composer dependencies and disable Telescope service provider

--- config/app.php
+++ config/app.php
@@ -201,7 +201,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
+        // App\Providers\TelescopeServiceProvider::class,
         /* Setup Wizard */
 
         /* End */
